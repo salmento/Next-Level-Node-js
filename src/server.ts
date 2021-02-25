852f17e0abf99afadec8a70eaced5d96869c3d74
Salmento Chitlango <49094323+salmento@users.noreply.github.com>
2021-02-25 12:50:50 +0200

    third lesson

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,3 @@
-import 'reflect-metadata';
-import express from 'express';
-import './database'
-import {router} from './router'
-const app = express();
+import { app } from './app';
 
-app.use(express.json())
-
-app.use(router)
-
-
-app.listen(3000,  ()=>console.log("Server is running!") )
+app.listen(3333, () => console.log('Server is running!'));
